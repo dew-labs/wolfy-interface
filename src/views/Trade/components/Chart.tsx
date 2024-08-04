311898030c0f8b07a93042ba0b7e977f43f17238
Trung Tran <thaitrung1604@gmail.com>
2024-08-05 00:27:33 +0700

    WIP: Add chart

--- src/views/Trade/components/Chart.tsx
+++ src/views/Trade/components/Chart.tsx
@@ -1,3 +1,23 @@
+import TVLightWeightChart from "@/views/Trade/components/TVChart/TVLightWeightChart.tsx";
+import {useCurrentTheme} from "@/lib/theme/useCurrentTheme.ts";
+
 export default function Chart() {
-  return <div className='w-full flex-1'>Chart</div>
+    const [currentTheme] = useCurrentTheme();
+
+    // const exampleSocket = new WebSocket(
+    //     "wss://fstream.binance.com/ws/ethusdt@kline_5m"
+    // );
+    //
+    // exampleSocket.onmessage = (event) => {
+    //     console.log(event.data);
+    // };
+
+    return (
+        <div className='w-full flex-1'>
+          <TVLightWeightChart
+
+          ></TVLightWeightChart>
+        </div>
+    )
+
 }
