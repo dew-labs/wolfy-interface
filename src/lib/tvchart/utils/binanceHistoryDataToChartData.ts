6770e20367fe8be6bdfff5b9aec5f99b13621ff1
Trung Tran <thaitrung1604@gmail.com>
2024-08-19 22:39:29 +0200

    ✨ feat(app): add chart

--- src/lib/tvchart/utils/binanceHistoryDataToChartData.ts
+++ src/lib/tvchart/utils/binanceHistoryDataToChartData.ts
@@ -1,18 +1,17 @@
+import type {ChartData} from '@/lib/tvchart/datafeed/Datafeed.ts'
+
 export default function binanceHistoryDataToChartData(binanceData: []) {
-    const result = [];
+  const result: ChartData[] = []
 
-    binanceData.forEach(candle => {
-        result.push({
-            time: candle[0] / 1000,
-            low: Number(candle[3]),
-            high: Number(candle[2]),
-            open: Number(candle[1]),
-            close: Number(candle[4]),
-        })
+  binanceData.forEach(candle => {
+    result.push({
+      time: candle[0] / 1000,
+      low: Number(candle[3]),
+      high: Number(candle[2]),
+      open: Number(candle[1]),
+      close: Number(candle[4]),
     })
+  })
 
-    console.log("HAHAHA binan", binanceData);
-    console.log("HAHAHA result", result);
-
-    return result;
+  return result
 }
