d5f69fb1e85c30c2129b6bf7cabc4a0870827cae
Trung Tran <thaitrung1604@gmail.com>
2024-08-19 22:39:29 +0200

    ✨ feat(app): add chart

--- src/lib/tvchart/utils/binanceDataToChartData.ts
+++ src/lib/tvchart/utils/binanceDataToChartData.ts
@@ -1,31 +1,35 @@
+import type {CandlestickData, UTCTimestamp} from 'lightweight-charts'
+
+import type {ChartInterval} from '@/lib/tvchart/datafeed/Datafeed.ts'
+import {ChartIntervalTime} from '@/lib/tvchart/datafeed/Datafeed.ts'
+
 export interface BinanceChartData {
-    e: string
-    E: number
-    k: {
-        s: string
-        c: number
-        o: number
-        h: number
-        l: number
-        v: number
-        q: number
-    }
+  e: string
+  E: number
+  k: {
+    s: string
+    c: number
+    o: number
+    h: number
+    l: number
+    v: number
+    q: number
+  }
 }
 
-export default function binanaceDataToChartData(binanceData: BinanceChartData) {
-    const coeff = 1000 * 60;
-    const date = new Date(binanceData.E);
-    const rounded = new Date(Math.round(date.getTime() / coeff) * coeff);
+export default function binanaceDataToChartData(
+  binanceData: BinanceChartData,
+  interval: ChartInterval,
+): CandlestickData<UTCTimestamp> {
+  const coeff = 1000 * 60 * ChartIntervalTime[interval]
+  const date = new Date(binanceData.E)
+  const rounded = new Date(Math.floor(date.getTime() / coeff) * coeff)
 
-
-    const result = {
-        time: rounded.getTime(),
-        open: Number(binanceData.k.o),
-        high: Number(binanceData.k.h),
-        low: Number(binanceData.k.l),
-        close: Number(binanceData.k.c),
-    };
-
-    console.log(result)
-    return result;
-};
+  return {
+    time: rounded.getTime() / 1000,
+    open: Number(binanceData.k.o),
+    high: Number(binanceData.k.h),
+    low: Number(binanceData.k.l),
+    close: Number(binanceData.k.c),
+  } as CandlestickData<UTCTimestamp>
+}
