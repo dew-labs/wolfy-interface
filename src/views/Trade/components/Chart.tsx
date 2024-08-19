8a2745bab353516141bc4524b6f3508d59549f4c
Trung Tran <thaitrung1604@gmail.com>
2024-08-19 22:39:29 +0200

    ✨ feat(app): add chart

--- src/views/Trade/components/Chart.tsx
+++ src/views/Trade/components/Chart.tsx
@@ -1,23 +1,37 @@
-import TVLightWeightChart from "@/views/Trade/components/TVChart/TVLightWeightChart.tsx";
-import {useCurrentTheme} from "@/lib/theme/useCurrentTheme.ts";
+import {Card} from '@nextui-org/react'
+import {useState} from 'react'
+import type {Key} from 'react-aria-components'
+
+import {Theme} from '@/lib/theme/theme.ts'
+import {useCurrentTheme} from '@/lib/theme/useCurrentTheme.ts'
+import {ChartInterval, isChartInterval} from '@/lib/tvchart/datafeed/Datafeed.ts'
+import TVLightWeightChart from '@/views/Trade/components/TVChart/TVLightWeightChart.tsx'
+import TVLightWeightTimeFrame from '@/views/Trade/components/TVChart/TVLightWeightTimeFrame.tsx'
 
 export default function Chart() {
-    const [currentTheme] = useCurrentTheme();
+  const [currentTheme] = useCurrentTheme()
+  const isDark = currentTheme === Theme.Dark
+  const [chartInterval, setChartInterval] = useState(ChartInterval['5m'])
 
-    // const exampleSocket = new WebSocket(
-    //     "wss://fstream.binance.com/ws/ethusdt@kline_5m"
-    // );
-    //
-    // exampleSocket.onmessage = (event) => {
-    //     console.log(event.data);
-    // };
+  function handleChartIntervalSelection(key: Key) {
+    if (isChartInterval(key)) {
+      setChartInterval(key)
+    }
+  }
 
-    return (
-        <div className='w-full flex-1'>
-          <TVLightWeightChart
-
-          ></TVLightWeightChart>
-        </div>
-    )
-
+  return (
+    <Card className='my-4 p-2'>
+      <div className='w-full flex-1'>
+        <TVLightWeightTimeFrame
+          selectedInterval={chartInterval}
+          onSelectInterval={handleChartIntervalSelection}
+        />
+        <TVLightWeightChart
+          textColor={isDark ? 'white' : 'black'}
+          gridColor={isDark ? '#ffffff1a' : ''}
+          interval={chartInterval}
+        ></TVLightWeightChart>
+      </div>
+    </Card>
+  )
 }
