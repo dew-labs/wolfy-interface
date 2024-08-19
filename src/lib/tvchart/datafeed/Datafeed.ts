c77179628ad41e6de684fdace3c4555691a56b47
Trung Tran <thaitrung1604@gmail.com>
2024-08-19 22:39:29 +0200

    ✨ feat(app): add chart

---
+++ src/lib/tvchart/datafeed/Datafeed.ts
@@ -0,0 +1,37 @@
+export enum ChartInterval {
+  '1m' = '1m',
+  '5m' = '5m',
+  '15m' = '15m',
+  '1h' = '1h',
+  '4h' = '4h',
+  '1d' = '1d',
+  '1w' = '1w',
+  '1M' = '1M',
+}
+
+export enum ChartIntervalTime {
+  '1m' = 1,
+  '5m' = 5,
+  '15m' = 15,
+  '1h' = 60,
+  '4h' = 240,
+  '1d' = 1440,
+  '1w' = 10080,
+  '1M' = 40320,
+}
+
+export interface ChartData {
+  time: number
+  low: number
+  high: number
+  open: number
+  close: number
+}
+
+export function isChartInterval(value: unknown): value is ChartInterval {
+  if (typeof value !== 'string') {
+    return false
+  }
+
+  return Object.values(ChartInterval).includes(value)
+}
--- src/views/Trade/components/TVChart/TVLightWeightChart.tsx
+++ src/views/Trade/components/TVChart/TVLightWeightChart.tsx
@@ -1,90 +1,117 @@
-import {ColorType, createChart} from 'lightweight-charts';
-import React, {useEffect, useRef} from 'react';
+import type {SeriesDataItemTypeMap, UTCTimestamp} from 'lightweight-charts'
+import {ColorType, createChart, LineStyle} from 'lightweight-charts'
+import React, {type LegacyRef, useEffect, useMemo, useRef} from 'react'
+
+import type {ChartData, ChartInterval} from '@/lib/tvchart/datafeed/Datafeed.ts'
 import binanaceDataToChartData, {
-    BinanceChartData
-} from "@/lib/tvchart/utils/binanceDataToChartData.ts";
-import binanceHistoryDataToChartData from "@/lib/tvchart/utils/binanceHistoryDataToChartData.ts";
+  type BinanceChartData,
+} from '@/lib/tvchart/utils/binanceDataToChartData.ts'
+import binanceHistoryDataToChartData from '@/lib/tvchart/utils/binanceHistoryDataToChartData.ts'
 
-function TVLightWeightChart(props) {
-    let initialData = [];
-    const {
-        colors: {
-            backgroundColor = 'white',
-            lineColor = '#2962FF',
-            textColor = 'black',
-            areaTopColor = '#2962FF',
-            areaBottomColor = 'rgba(41, 98, 255, 0.28)',
-        } = {},
-    } = props;
+function TVLightWeightChart(props: {
+  textColor: string
+  gridColor: string
+  interval: ChartInterval
+}) {
+  const chartStyle = useMemo(
+    () => ({
+      backgroundColor: 'transparent',
+      lineColor: '#2962FF',
+      textColor: props.textColor,
+      areaTopColor: '#2962FF',
+      areaBottomColor: 'rgba(41, 98, 255, 0.28)',
+      gridColor: props.gridColor,
+    }),
+    [props.textColor, props.gridColor],
+  )
 
-    const chartContainerRef = useRef();
+  const chartContainerRef = useRef<HTMLElement | string>('')
 
-    useEffect(
-        () => {
-            const chart = createChart(chartContainerRef.current, {
-                layout: {
-                    background: { type: ColorType.Solid, color: backgroundColor },
-                    textColor,
-                },
-                width: chartContainerRef.current.clientWidth,
-                height: 300,
-            });
-            const handleResize = () => {
-                chart.applyOptions({ width: chartContainerRef.current.clientWidth });
-            };
-
-            const response = async () => {
-                await fetch("https://testnet.binancefuture.com/fapi/v1/continuousKlines?" + new URLSearchParams({
-                    pair: "ethusdt",
-                    contractType: "PERPETUAL",
-                    interval: "5m",
-                    limit: 1000
-                }).toString()).then(response => {
-                    return response.json();
-                }).then(data => {
-                    if (data) {
-                        initialData = binanceHistoryDataToChartData(data);
-                    }
-                    console.log("HAHAHA hehe", initialData)
-                });
-
-                chart.timeScale().fitContent();
-                chart.timeScale().scrollToPosition(5, false);
-
-                const newSeries = chart.addCandlestickSeries({ upColor: '#26a69a', downColor: '#ef5350', borderVisible: false,
-                    wickUpColor: '#26a69a', wickDownColor: '#ef5350'});
-                newSeries.setData(initialData);
-                const exampleSocket = new WebSocket(
-                    "wss://fstream.binance.com/ws/ethusdt@kline_5m"
-                );
-
-                exampleSocket.onmessage = (event) => {
-                    console.log();
-                    const binanceData = JSON.parse(event.data);
-
-                    if (binanceData) {
-                        newSeries.update(binanaceDataToChartData(binanceData));
-                    }
-                };
-
-                window.addEventListener('resize', handleResize);
-            }
-            response();
-
-            return () => {
-                window.removeEventListener('resize', handleResize);
-
-                chart.remove();
-            };
+  useEffect(() => {
+    let initialData: ChartData[] = []
+    const chart = createChart(chartContainerRef.current, {
+      layout: {
+        background: {type: ColorType.Solid, color: chartStyle.backgroundColor},
+        textColor: chartStyle.textColor,
+      },
+      grid: {
+        vertLines: {
+          color: chartStyle.gridColor,
+          style: LineStyle.Dashed,
         },
-        [initialData, backgroundColor, lineColor, textColor, areaTopColor, areaBottomColor]
-    );
+        horzLines: {
+          color: chartStyle.gridColor,
+          style: LineStyle.Dashed,
+        },
+      },
+      rightPriceScale: {
+        borderVisible: false,
+      },
+      timeScale: {
+        borderVisible: false,
+      },
+      width: (chartContainerRef.current as HTMLElement).clientWidth,
+      height: 300,
+    })
+    const handleResize = () => {
+      chart.applyOptions({width: (chartContainerRef.current as HTMLElement).clientWidth})
+    }
 
-    return (
-        <div
-            ref={chartContainerRef}
-        />
-    );
+    const fetchChartData = async () => {
+      await fetch(
+        'https://testnet.binancefuture.com/fapi/v1/markPriceKlines?' +
+          new URLSearchParams({
+            symbol: 'ethusdt',
+            interval: props.interval,
+          }).toString(),
+      )
+        .then(async response => {
+          return response.json()
+        })
+        .then(data => {
+          if (data) {
+            initialData = binanceHistoryDataToChartData(data as [])
+          }
+        })
+
+      chart.timeScale().scrollToPosition(5, false)
+
+      const newSeries = chart.addCandlestickSeries({
+        upColor: '#26a69a',
+        downColor: '#ef5350',
+        borderVisible: false,
+        wickUpColor: '#26a69a',
+        wickDownColor: '#ef5350',
+      })
+      newSeries.setData(initialData as SeriesDataItemTypeMap<UTCTimestamp>['Candlestick'][])
+      const exampleSocket = new WebSocket(
+        'wss://fstream.binance.com/ws/ethusdt@kline_' + props.interval,
+      )
+
+      exampleSocket.onmessage = event => {
+        if (typeof event.data !== 'string') {
+          return
+        }
+
+        const binanceData = JSON.parse(event.data)
+
+        if (binanceData) {
+          newSeries.update(binanaceDataToChartData(binanceData as BinanceChartData, props.interval))
+        }
+      }
+
+      window.addEventListener('resize', handleResize)
+    }
+    void fetchChartData()
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+
+      chart.remove()
+    }
+  }, [chartStyle, props.interval])
+
+  return <div ref={chartContainerRef as LegacyRef<HTMLDivElement>} />
 }
 
-export default TVLightWeightChart;
+export default TVLightWeightChart
