9afc33e4b26dee005e4103e2a1095ab8010587d3
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2024-10-18 10:54:29 +0700

    ✨ feat(app): support bigint in json response

--- src/utils/api/call.ts
+++ src/utils/api/call.ts
@@ -1,10 +1,22 @@
 import axios from 'axios'
+import jsonBig from 'json-bigint'
 
 import isAuthenticated from '@/auth/isAuthenticated'
 import isAuthenticatedPubSubChannel from '@/auth/isAuthenticatedPubSubChannel'
 
 const call = axios.create({
   adapter: 'fetch',
+  transformResponse: data => {
+    if (typeof data === 'string') {
+      try {
+        return jsonBig().parse(data)
+      } catch {
+        /* Ignore */
+      }
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return -- this is the default behavior
+    return data
+  },
 })
 
 call.interceptors.response.use(function (response) {
